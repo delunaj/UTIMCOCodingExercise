16693b06cd752c0d93c6364c3f4ccd069be0f9a3
Jana <janadeluna@sbcglobal.net>
2020-01-16 12:13:05 -0600

    Adding Models

---
+++ UTIMCOCodingExercise.Web/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UTIMCOCodingExercise.Models
+{
+    public class Menu
+    {
+        public string Header { get; set; }
+        public List<Item> Items { get; set; }
+    }
+}
---
+++ UTIMCOCodingExercise.Web/Models/RootObject.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UTIMCOCodingExercise.Models
+{
+    public class RootObject 
+    {
+        [JsonProperty("menu")]
+        public Menu Menu { get; set; }
+    }
+}
