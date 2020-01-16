c88676644dcdf19cc33fa7bfd6047fa2e99d9a85
Jana <janadeluna@sbcglobal.net>
2020-01-16 12:14:21 -0600

    uploading pages files

---
+++ UTIMCOCodingExercise.Web/Pages/DataSet.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using UTIMCOCodingExercise.Models;
+using UTIMCOCodingExercise.Services;
+
+namespace UTIMCOCodingExercise.Pages
+{
+    public class DataSetModel : PageModel
+    {
+        private readonly ILogger<DataSetModel> _logger;
+        public JsonMenuService MenuService;
+        public IEnumerable<RootObject> MenuCollection { get; private set; }
+
+        public DataSetModel(ILogger<DataSetModel> logger, JsonMenuService menuService)
+        {
+            _logger = logger;
+            MenuService = menuService;
+        }
+
+        public void OnGet() => MenuCollection = MenuService.GetMenus();
+        
+    }
+}
---
+++ UTIMCOCodingExercise.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using UTIMCOCodingExercise.Models;
+using UTIMCOCodingExercise.Services;
+
+namespace UTIMCOCodingExercise.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonMenuService MenuService;
+        public IEnumerable<RootObject> MenuCollection { get; private set; }
+
+        //constructor - accepts JsonMenuService param to decouple data source access
+        public IndexModel(ILogger<IndexModel> logger, JsonMenuService menuService)
+        {
+            _logger = logger;
+            MenuService = menuService;
+        }
+
+        public void OnGet() => MenuCollection = MenuService.GetMenus();
+
+    }
+}
