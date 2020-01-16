11d26a5e6ff6ad43a85d19c27825b4782b0dcabd
Jana <janadeluna@sbcglobal.net>
2020-01-16 12:11:15 -0600

    Adding MenusController

---
+++ UTIMCOCodingExercise.Web/Controllers/MenusController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UTIMCOCodingExercise.Models;
+using UTIMCOCodingExercise.Services;
+
+namespace UTIMCOCodingExercise.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MenusController : ControllerBase
+    {
+        public MenusController(JsonMenuService menuService)
+        {
+            this.MenuService = menuService;
+        }
+
+        public JsonMenuService MenuService { get; }
+
+        [HttpGet]
+        public IEnumerable<RootObject> Get()
+        {
+            return MenuService.GetMenus();
+        }
+    }
+}
